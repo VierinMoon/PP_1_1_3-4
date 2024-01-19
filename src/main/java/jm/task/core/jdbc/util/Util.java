8076fd062f4de478710e8691f4e2090d7c1b5495
VierinMoon <101812476+VierinMoon@users.noreply.github.com>
2024-01-19 12:39:29 +0300

    Creat connection
    add  at class Util getConnection method with usage SingleTone

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.*;
+
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "RomanRoot";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        return connection;
+    }
 }
+
+
+
+
+//            statement.addBatch("insert into users (name, age, email) values('Koly', 20, 'kolia@devcolibri.com')");
+//            statement.execute();
+//            statement.executeUpdate();
+//            statement.clearBatch();
